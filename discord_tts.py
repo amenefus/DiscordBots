9d538a2a22be111bac3cba1903bcac42892e8186
amenefus <miki369@gmail.com>
2024-04-22 01:27:03 +0300

    Added an option to get the time in IL and at Austin Texas.

--- discord_tts.py
+++ discord_tts.py
@@ -6,7 +6,7 @@ from gtts import gTTS
 import requests
 import os
 from datetime import datetime, date
-
+import pytz
 
 # Initialize the bot with intents
 intents = discord.Intents.default()
@@ -85,6 +85,67 @@ async def shoulder(ctx):
 
 
 # ====================== END VOICE CHANNEL OPERATIONS ====================== #
+# ====================== Time Teller ====================== #
+
+@bot.command()
+async def timeil(ctx):
+    tz = pytz.timezone('Israel')
+    current_time = datetime.now(tz)
+    amassage = f"השעה עכשיו היא  {current_time.strftime('%I:%M')} בחור תחת הזה"
+    language = 'iw'
+    tts = gTTS(text=amassage, lang=language, slow=False)
+    tts.save('time.mp3')
+
+    channel_id = 1082952248860102666  # Replace with your voice channel ID
+    file_path = 'time.mp3'  # Replace with the actual path to your MP3 file
+    channel = bot.get_channel(channel_id)
+    if not channel:
+        return print('Invalid voice channel ID.')
+    voice_client = await channel.connect()
+    source = discord.FFmpegPCMAudio(file_path)
+    voice_client.play(source, after=lambda e: print('done', e))
+    # Wait for the sound to finish playing
+    while voice_client.is_playing():
+        await asyncio.sleep(1)
+    
+    # Disconnect after playing the sound
+    while voice_client.is_playing():
+        await asyncio.sleep(1)
+    
+    # Check if the bot is connected before disconnecting
+    if voice_client.is_connected():
+        await voice_client.disconnect()
+
+@bot.command()
+async def timetx(ctx):
+    tz = pytz.timezone('America/Chicago')
+    current_time = datetime.now(tz)
+    amassage = f"השעה עכשיו היא {current_time.strftime('%H:%M')} אצל שימי."
+    language = 'iw'
+    tts = gTTS(text=amassage, lang=language, slow=False)
+    tts.save('time.mp3')
+
+    channel_id = 1082952248860102666  # Replace with your voice channel ID
+    file_path = 'time.mp3'  # Replace with the actual path to your MP3 file
+    channel = bot.get_channel(channel_id)
+    if not channel:
+        return print('Invalid voice channel ID.')
+    voice_client = await channel.connect()
+    source = discord.FFmpegPCMAudio(file_path)
+    voice_client.play(source, after=lambda e: print('done', e))
+    # Wait for the sound to finish playing
+    while voice_client.is_playing():
+        await asyncio.sleep(1)
+    
+    # Disconnect after playing the sound
+    while voice_client.is_playing():
+        await asyncio.sleep(1)
+    
+    # Check if the bot is connected before disconnecting
+    if voice_client.is_connected():
+        await voice_client.disconnect()
+
+
 # ====================== BOT STREAM ====================== #
 
 
